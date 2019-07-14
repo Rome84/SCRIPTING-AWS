321eda1f5323b68099b22906777bdcdc0cf2718c
ROME84 <22250311+Rome84@users.noreply.github.com>
2019-07-13 21:12:28 -0400

    Update list_instances.py

--- list_instances.py
+++ list_instances.py
@@ -5,16 +5,19 @@
 # Finally, we’ll use the EC2 resource to get all of the instances and then print their instance ID and state. 
 # Here’s what the script looks like:
 ####################################################################################################################
+vi list_instances.py
 #!/usr/bin/env python
 import boto3
 ec2 = boto3.resource('ec2')
 for instance in ec2.instances.all():
     print instance.id, instance.state
     
+##########################################################################################################3
 # Save the lines above into a file named list_instances.py and change the mode to executable. 
 # That will allow you to run the script directly from the command line. 
 # Also note that you’ll need to edit and chmod +x the remaining scripts to get them running as well. 
 # In this case, the procedure looks like this:
+#########################################################################################################
 $ vi list_instances.py
 $ chmod +x list_instances.py
 $ ./list_instances.py
