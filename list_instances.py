0f5e58341ef1fd349cecb1004f53ea974f371530
ROME84 <22250311+Rome84@users.noreply.github.com>
2019-07-13 21:00:48 -0400

    Update list_instances.py

--- list_instances.py
+++ list_instances.py
@@ -1,3 +1,10 @@
+# LIST INSTANCES
+# For our first script, let’s list the instances we have running in EC2. We can get this information with just a few short lines of code.
+# First, we’ll import the boto3 library. Using the library, we’ll create an EC2 resource. 
+# This is like a handle to the EC2 console that we can use in our script. 
+# Finally, we’ll use the EC2 resource to get all of the instances and then print their instance ID and state. 
+# Here’s what the script looks like:
+####################################################################################################################
 #!/usr/bin/env python
 import boto3
 ec2 = boto3.resource('ec2')
